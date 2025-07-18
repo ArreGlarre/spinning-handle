c823e6ce7f0e9ba23a1d680efae2140734105366
arvidwg <arvid@kth.se>
2025-07-19 00:12:31 +0200

    Made a skeleton for handle.c and continued on da README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# spinning-handle
-An attempt at simulating a rotating body using Euler angles.
+# Spinning Handle in Space
+This project is an attempt at simulating a rotating body using Euler angles.
 
 The goal of this projext is to observe the "tennis racket effekt" by simulating a spinning handle in zero gravity 
 using 3D rigid body dynamics. 
@@ -8,8 +8,16 @@ using 3D rigid body dynamics.
 
 This will be done from scratch using the c programming language that i started learning recently. 
 
+Probably also worth noting that this was made with GCC compiler on Windows 11 with WSL 2.
+
 ## Theory
+ * Euler angles
+ * Inertia & momenta
+ * Euler kinematics
+ * RK4 discretization
 
 ## Implementation
+Will document what is contained in the different files
 
 ## Result
+A little summaty plus some gifs of my simulations working hopefully.
--- handle.c
+++ handle.c
@@ -2,6 +2,12 @@
 Here is where the magic is gonna happen :)
 
 */
+// INCLUDE STATEMENTS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "ascii_render.h"
 
 // STRUCTS
 
@@ -17,3 +23,43 @@ int main()
 }
 
 // FUNCTIONS
+
+struct Surface *cuboid()
+{
+    // Returns a pointer to a cuboid model.
+}
+
+struct Surface *cylinder()
+{
+    // Returns a pointer to a cylinder model.
+}
+
+struct Surface *handle()
+{
+    // Returns a pointer to a Handle, made up of a cylinder and a cuboid
+}
+
+struct matrix *I_handle()
+{
+    // Returns the Inertia tensor of a handle with homogenous mass dist.
+}
+
+float *COM()
+{
+    // Calculates the center of mass of the handle.
+}
+
+float *f()
+{
+    // the time derivative of the state-vector, given, the state vector.
+}
+
+void RK4_integrate()
+{
+    // Integrate a timestep using RK4 integration
+}
+
+void f_euler_integrate()
+{
+    // integrate a timestep using forward euler.
+}
